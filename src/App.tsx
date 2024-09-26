e2458243e2c9049cc3b7f21312d52d8aaca1a6e1
Jesse Fischer <jesse@jessefischer.com>
2024-09-25 21:59:13 -0400

    feat: begin UI and integration to MB

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,149 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState } from "react";
+
+import "./App.css";
+
+const MB_API_ROOT = "https://musicbrainz.org/ws/2/";
+const ENTITY_LOOKUP_PATH = (entity_type: string) => `${entity_type}/`;
+const ADDITIONAL_PARAMS = {
+  fmt: "json",
+  inc: "url-rels+recording-rels+artist-credits",
+};
+
+type TAppState = "idle" | "loading" | "success" | "error";
+
+type TMusicBrainzEntity = {
+  id: string;
+  title: string;
+  "artist-credit": {
+    artist: {
+      id: string;
+      name: string;
+    }[];
+  }[];
+  relations: {
+    type: string;
+    direction: string;
+    recording: any;
+  }[];
+};
+
+const additionalParamsString = Object.entries(ADDITIONAL_PARAMS)
+  .map(([key, value]) => `${key}=${value}`)
+  .join("&");
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, setState] = useState<TAppState>("idle");
+  const [entityId, setEntityId] = useState("");
+  const [data, setData] = useState<TMusicBrainzEntity>();
+  const [error, setError] = useState<unknown>(null);
+  const [backwardLinks, setBackwardLinks] = useState<TMusicBrainzEntity[]>([]);
+  const [forwardLinks, setForwardLinks] = useState<TMusicBrainzEntity[]>([]);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e?.preventDefault();
+    setState("loading");
+    fetchEntity(entityId);
+  };
+
+  const fetchEntity = async (entityId: string) => {
+    try {
+      const response = await fetch(
+        `${MB_API_ROOT}${ENTITY_LOOKUP_PATH(
+          "recording"
+        )}${entityId}?${additionalParamsString}`
+      );
+      const data = await response.json();
+      setData(data);
+      setState("success");
+      const backwardLinks = [];
+      const forwardLinks = [];
+      for (let i = 0; i < data.relations.length; i++) {
+        if (data.relations[i].type === "samples material") {
+          if (data.relations[i].direction === "backward") {
+            backwardLinks.push(data.relations[i].recording);
+          } else if (data.relations[i].direction === "forward") {
+            forwardLinks.push(data.relations[i].recording);
+          }
+        }
+      }
+      setBackwardLinks(backwardLinks);
+      setForwardLinks(forwardLinks);
+    } catch (error) {
+      setError(error);
+      setState("error");
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      {state === "idle" && (
+        <form className="Form">
+          <input
+            type="text"
+            placeholder="Enter a MusicBrainz Recording ID"
+            value={entityId}
+            onChange={(e) => setEntityId(e.target.value)}
+          />
+          <input type="submit" value="Submit" onClick={handleSubmit} />
+        </form>
+      )}
+      {state === "success" && !!data && (
+        <>
+          <div className="ForwardLinks">
+            {forwardLinks.map((link: any) => (
+              <button
+                onClick={() => {
+                  setEntityId(link.id);
+                  fetchEntity(link.id);
+                }}
+              >
+                <div className="Card Link" key={link.id}>
+                  <div className="Title">{link.title}</div>
+                  <div className="Artists">
+                    {link["artist-credit"].map((artist: any) => (
+                      <div key={artist.artist.id}>{artist.artist.name}</div>
+                    ))}
+                  </div>
+                </div>
+              </button>
+            ))}
+          </div>
+
+          <div className="Card">
+            <div className="Title">{data.title}</div>
+            <div className="Artists">
+              {data["artist-credit"].map((artist: any) => (
+                <div key={artist.artist.id}>{artist.artist.name}</div>
+              ))}
+            </div>
+          </div>
+          <div className="BackwardLinks">
+            {backwardLinks.map((link: any) => (
+              <button
+                onClick={() => {
+                  setEntityId(link.id);
+                  fetchEntity(link.id);
+                }}
+              >
+                <div className="Card Link" key={link.id}>
+                  <div className="Title">{link.title}</div>
+                  <div className="Artists">
+                    {link["artist-credit"].map((artist: any) => (
+                      <div key={artist.artist.id}>{artist.artist.name}</div>
+                    ))}
+                  </div>
+                </div>
+              </button>
+            ))}
+          </div>
+        </>
+      )}
+      {state === "error" && (
+        <div className="Error">{JSON.stringify(error)}</div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
