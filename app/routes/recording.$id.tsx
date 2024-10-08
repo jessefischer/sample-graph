12931faf6b187f61ecd8ebaa19142add14f1a9ff
Jesse Fischer <59373483+jessefischer@users.noreply.github.com>
2024-10-07 23:10:41 -0400

    Add stars to background

--- app/routes/recording.$id.tsx
+++ app/routes/recording.$id.tsx
@@ -6,6 +6,7 @@ import { Howl } from "howler";
 
 import { RecordingCard } from "~/components/RecordingCard";
 import { fetchMusicBrainzEntity } from "~/utils/fetchMusicBrainzEntity";
+import { TStar } from "~/types";
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const entityId = params.id;
@@ -18,7 +19,7 @@ export async function loader({ params }: LoaderFunctionArgs) {
   }
 }
 
-export const meta: MetaFunction<typeof loader> = ({data}) => {
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
   const title = `${data?.data.title} - ${data?.data["artist-credit"][0].artist.name} | Sample Graph Explorer`;
   return [
     { title },
@@ -38,6 +39,26 @@ export default function Recording() {
   const soundRef = useRef<Howl | null>(null);
   const [initialized, setInitialized] = useState(false);
   const [playing, setPlaying] = useState(false);
+  const [windowSize, setWindowSize] = useState<{
+    width: number;
+    height: number;
+  }>();
+  const [stars, setStars] = useState<TStar[]>();
+
+  useEffect(
+    () =>
+      setStars(
+        Array(35)
+          .fill(null)
+          .map(() => ({
+            x: Math.random() * window.innerWidth,
+            y: Math.random() * window.innerHeight,
+            r: Math.random() * 5,
+            a: Math.random(),
+          }))
+      ),
+    []
+  );
 
   useEffect(() => {
     if (data.audioUrl && initialized) {
@@ -57,8 +78,41 @@ export default function Recording() {
     };
   }, [data.audioUrl, initialized]);
 
+  useEffect(() => {
+    const updateWindowSize = () =>
+      setWindowSize({
+        height: window.innerHeight,
+        width: window.innerWidth,
+      });
+    updateWindowSize();
+    window.addEventListener("resize", updateWindowSize);
+    return () => window.removeEventListener("resize", updateWindowSize);
+  }, []);
+
   return (
     <motion.div className="app">
+      {windowSize && (
+        <svg
+          style={{ position: "absolute", top: 0, left: 0 }}
+          width={windowSize.width}
+          height={windowSize.height}
+          viewBox={`0 0 ${windowSize.width} ${windowSize.height}`}
+        >
+          <filter id="blur">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="1" />
+          </filter>
+          {stars?.map((star, i) => (
+            <circle
+              key={i}
+              fill={`rgb(255,255,255,${star.a}`}
+              cx={star.x}
+              cy={star.y}
+              r={star.r}
+              filter="url(#blur)"
+            />
+          ))}
+        </svg>
+      )}
       <LayoutGroup>
         <div className="forwardLinks">
           <AnimatePresence>
